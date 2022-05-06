6d19e5a86f81ba42506708a8f6903c159c81a638
khansolo19 <chyngyz.tangatarov.work@gmail.com>
2022-05-06 14:57:59 +0600

    created order

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('order/', include('order.urls')),
+    path('cart/', include('cart.urls')),
     path('account/', include('account.urls')),
+    path('', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
